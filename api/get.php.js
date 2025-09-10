9a95f86916c53975fb7c1ed2677a6bdaa20751a2
Barisha <appbarisha@gmail.com>
2025-09-11 01:42:25 +0300

    Delete api directory

--- api/get.php.js
+++
@@ -1,7 +0,0 @@
-// .php isteyen player'lar için alias
-export { default, config } from "./get.js";
-// api/get.php.js
-// Eski Xtream formatı isteyen uygulamalar için /get.php proxy'si
-import handler from "./get";
-export const config = { runtime: "edge" };
-export default handler;
