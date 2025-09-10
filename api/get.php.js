634fd47137fd60136bc25b0880337026dc899783
Barisha <appbarisha@gmail.com>
2025-09-11 01:32:08 +0300

    Update get.php.js

--- api/get.php.js
+++ api/get.php.js
@@ -1,3 +1,7 @@
-// api/get.php.js
-// Xtream/Smarter tarzı /get.php isteyen uygulamalar için alias
+// .php isteyen player'lar için alias
 export { default, config } from "./get.js";
+// api/get.php.js
+// Eski Xtream formatı isteyen uygulamalar için /get.php proxy'si
+import handler from "./get";
+export const config = { runtime: "edge" };
+export default handler;
